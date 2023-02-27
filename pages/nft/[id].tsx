1f8048c9a18fac9f69aecfab2447445d82b2ece7
AouIssa <spunkyguyyt@gmail.com>
2023-02-27 10:16:30 +0800

    Updated colors

---
+++ pages/nft/[id].tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function NFTDroppage() {
+  return (
+    <div className='flex h-screen flex-col'>
+      {/*Left side of screen */}
+      <div className='bg-gradient-to-br from-red-500 to-blue-500'>
+        <div>hello
+        </div>
+      </div>
+      {/*Right side of screen */}
+      <div></div>
+    </div>
+  );
+}
+
+export default NFTDroppage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
@@ -6,7 +9,11 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ...colors,
+      },
+    },
   },
   plugins: [],
 }
