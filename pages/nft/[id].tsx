35c3e27aa584638f07992d8f9fb650bf374ca749
AouIssa <spunkyguyyt@gmail.com>
2023-02-27 10:28:26 +0800

    update

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -2,10 +2,20 @@ import React from "react";
 
 function NFTDroppage() {
   return (
-    <div className='flex h-screen flex-col'>
+    <div className="flex h-screen flex-col">
       {/*Left side of screen */}
-      <div className='bg-gradient-to-br from-red-500 to-blue-500'>
-        <div>hello
+      <div className="bg-gradient-to-br from-cyan-500 to-rose-500">
+        <div>
+          <img className="w-44 rounded-xl object-cover lg:h-96 lg:w-72" 
+          src="https://i.imgur.com/hZ1oaQd.jpg" alt="" />
+          <div>
+            <h1 className="text-4xl font-bold text-white">
+                Aous Ape
+            </h1>
+            <h2 className="text-xl text-gray-300">
+                A collection of Aous Apes who lives and breath react
+            </h2>
+          </div>
         </div>
       </div>
       {/*Right side of screen */}
