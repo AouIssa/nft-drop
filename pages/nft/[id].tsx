9c265419ed36f1ded077147b019d7007dd5540c3
AouIssa <spunkyguyyt@gmail.com>
2023-02-27 10:43:26 +0800

    update

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -5,15 +5,21 @@ function NFTDroppage() {
     <div className="flex h-screen flex-col">
       {/*Left side of screen */}
       <div className="bg-gradient-to-br from-cyan-500 to-rose-500">
-        <div>
-          <img className="w-44 rounded-xl object-cover lg:h-96 lg:w-72" 
-          src="https://i.imgur.com/hZ1oaQd.jpg" alt="" />
+        <div className="flex flex-col items-center justify-center py-2 lg:min-h-screen">
+          <div
+            className="bg-gradient-to-br
+           from-yellow-400 to-purple-600 p-2 rounded-xl"
+          >
+            <img
+              className="w-44 rounded-xl object-cover lg:h-96 lg:w-72"
+              src="https://i.imgur.com/hZ1oaQd.jpg"
+              alt=""
+            />
+          </div>
           <div>
-            <h1 className="text-4xl font-bold text-white">
-                Aous Ape
-            </h1>
+            <h1 className="text-4xl font-bold text-white">Aous Ape</h1>
             <h2 className="text-xl text-gray-300">
-                A collection of Aous Apes who lives and breath react
+              A collection of Aous Apes who lives and breath react
             </h2>
           </div>
         </div>
