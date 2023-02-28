a337285afdc95e33cdd47f0be6cb1b5e9c18fb94
AouIssa <spunkyguyyt@gmail.com>
2023-02-28 22:03:13 +0800

    integrated MetaMask for the website

--- pages/nft/[id].tsx
+++ pages/nft/[id].tsx
@@ -1,6 +1,12 @@
 import React from "react";
+import { useAddress, useDisconnect, useMetamask } from "@thirdweb-dev/react";
 
 function NFTDroppage() {
+  //auth
+  const connectWithMetamask = useMetamask();
+  const address = useAddress();
+  const disconnect = useDisconnect();
+
   return (
     <div className="flex h-screen flex-col lg:grid lg:grid-cols-10">
       {/*Left side of screen */}
@@ -33,6 +39,7 @@ function NFTDroppage() {
             Coding practice place
           </h1>
           <button
+            onClick={() => connectWithMetamask()}
             className="rounded-full bg-rose-400
            text-white px-4 py-2 text-xs font-bold lg:px-5 lg:py-3 lg:text-base"
           >
@@ -59,9 +66,11 @@ function NFTDroppage() {
           <p className="pt-2 text-xl text-green-500">13/20 NFT's Claimed</p>
         </div>
         {/*Mint Button */}
-        <button className="h-16 w-full
+        <button
+          className="h-16 w-full
          bg-red-600 text-white rounded-full mt-10
-         font-bold">
+         font-bold"
+        >
           Mint NFT (0.01 ETH)
         </button>
       </div>
