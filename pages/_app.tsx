6686cdee5c548ca4b9c8b7acfe6299da1664a7be
AouIssa <spunkyguyyt@gmail.com>
2023-03-01 10:41:09 +0800

    Fixed bugs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,11 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { Gnosis } from "@thirdweb-dev/chains";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThirdwebProvider activeChain={ChainId.Rinkeby}>
+    <ThirdwebProvider activeChain={Gnosis}>
       <Component {...pageProps} />
     </ThirdwebProvider>
   );
