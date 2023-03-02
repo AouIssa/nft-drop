2fedcd0fe44da2d9b66a364dcae2c5fce486ecdd
AouIssa <spunkyguyyt@gmail.com>
2023-03-02 15:57:48 +0800

    Modified the Sanity to suit the product

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,11 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
-import { Gnosis } from "@thirdweb-dev/chains";
+import { Polygon } from "@thirdweb-dev/chains";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThirdwebProvider activeChain={Gnosis}>
+    <ThirdwebProvider activeChain={Polygon}>
       <Component {...pageProps} />
     </ThirdwebProvider>
   );
